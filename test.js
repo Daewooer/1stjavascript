ce89c88070f24dc5704536ffe0c759fbfa91844f
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-03-22 10:13:54 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,30 @@
+
+const score = 420;
+
+
+console.log(score);
+
+// String, Numbers, Boolen, null, undefine, 
+
+const name = 'Kostas'; 
+const isCool = true; 
+const rating = 4.5; 
+const x = null; 
+const y = undefined;
+const age = 69;
+const age1 = 420;
+
+//console.log(typeof age1);
+//console.log(typeof x)
+//console.log(typeof rating); 
+//console.log(typeof x);
+//console.log(typeof y);
+
+console.log('my name is ' + name)
+
+const hello = `My name is ${name}`;
+
+console.log(hello)
+
+const s = 'hello!';
+console.log(s.length)
